e09b9b9d95d8fa0cb987279f478aeb1a2ee86c27
MrFlynn <nick@pleatsikas.me>
2019-11-20 04:36:28 +0000

    Adding base code for base-station.

--- base-station/main.cpp
+++ base-station/main.cpp
@@ -1,28 +1,48 @@
 #include <thread>
 #include <chrono>
+#include <iostream>
 
-#include <bcm2835.h>
 #include <RH_RF69.h>
+
+extern "C" {
+#include <wiringPi.h>
+}
 
 #define FREQUENCY 915.0
 
-RH_RF69 rf69(BCM2835_SPI_CS0, RPI_V2_GPIO_P5_06);
+#define CS 10
+#define INT 6
+#define RST 5
+
+RH_RF69 rf69(CS, INT);
+
+void reset_radio(void) {
+    pinMode(RST, OUTPUT);
+
+    digitalWrite(RST, HIGH);
+    delay(10);
+    digitalWrite(RST, LOW);
+    delay(10);
+}
 
 int main(void) {
-    uint8_t * data = 1;
+    RasPiSetup();
+
+    reset_radio();
 
     if (!rf69.init()) {
-        std::cout << "Radio failed to initialize" << std::endl;
+        std::cout << "Radio failed to initialize." << std::endl;
         return 1;
     }
 
     if (!rf69.setFrequency(FREQUENCY)) {
-        std::cout << "Failed to set frequency of radio" << std::endl;
+        std::cout << "Failed to set frequency of radio." << std::endl;
         return 1;
     }
 
     while (true) {
-        rf69.send(data, sizeof(uint8_t));
+        std::cout << "Sending packets" << std::endl;
+        rf69.send((uint8_t *)1, sizeof(uint8_t));
         rf69.waitPacketSent();
 
         std::this_thread::sleep_for(std::chrono::seconds(1));
