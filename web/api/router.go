057478fd0d64960955bf5d028a2bc4de54f3cc49
MrFlynn <nick@pleatsikas.me>
2019-11-25 23:09:07 -0800

    Updating router and main.
    Router now includes more comprehensive routes and more complete handler.
    Main now includes better handling of OS interrupts.

--- web/api/router.go
+++ web/api/router.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"strconv"
@@ -12,41 +13,89 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func startAndServe() {
-	handler := database.New("./example.db")
+var dbhandler *database.DBHandler
+
+func singleDeviceHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.ParseInt(mux.Vars(r)["id"], 10, 8)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("ID %d is not between 0 and 255", id), http.StatusBadRequest)
+		return
+	}
+
+	device, err := dbhandler.GetDevice(uint8(id))
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Could not find device with ID %d", id), http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	json.NewEncoder(w).Encode(device)
+
+	log.Printf("Sucessfully got device with ID %d", id)
+}
+
+func allDeviceHandler(w http.ResponseWriter, r *http.Request) {
+	devices, err := dbhandler.GetAllDevices()
+	if err != nil {
+		log.Println("Unable to get list of devices.")
+		http.Error(w, "Coud not get a list of devices", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	json.NewEncoder(w).Encode(devices)
+
+	log.Println("Sucessfully got a list of all devices.")
+}
+
+func updateDevices(w http.ResponseWriter, r *http.Request) {
+	var devices []database.Device
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Malformed JSON request", http.StatusBadRequest)
+		return
+	}
+
+	defer r.Body.Close()
+
+	err = json.Unmarshal(body, &devices)
+	if err != nil {
+		http.Error(w, "Could not process JSON request. Please check format", http.StatusBadRequest)
+		return
+	}
+
+	err = dbhandler.UpdateDevices(devices)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("%s", err), http.StatusBadRequest)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+}
+
+func createServer() *http.Server {
+	log.Println("Creating new database connection...")
+
+	h := database.New("./example.db")
+	dbhandler = &h
+
+	log.Println("Creating new router...")
 
 	router := mux.NewRouter()
-	router.HandleFunc("/device/{id: [0-9]+}", func(w http.ResponseWriter, r *http.Request) {
-		id, err := strconv.ParseInt(mux.Vars(r)["id"], 10, 8)
-		if err != nil {
-			http.Error(w, fmt.Sprintf("ID %d is not between 0 and 255", id), http.StatusBadRequest)
-			return
-		}
-
-		device, err := handler.GetDevice(uint8(id))
-		if err != nil {
-			http.Error(w, fmt.Sprintf("Could not find device with ID %d", id), http.StatusNotFound)
-			return
-		}
-
-		json.NewEncoder(w).Encode(device)
-	}).Methods(http.MethodGet)
-
-	router.HandleFunc("/device/all", func(w http.ResponseWriter, r *http.Request) {
-		devices, err := handler.GetAllDevices()
-		if err != nil {
-			http.Error(w, "Coud not get a list of devices", http.StatusInternalServerError)
-		}
-
-		json.NewEncoder(w).Encode(devices)
-	}).Methods(http.MethodGet)
+	router.HandleFunc("/device/{id:[0-9]+}", singleDeviceHandler).Methods(http.MethodGet)
+	router.HandleFunc("/device/all", allDeviceHandler).Methods(http.MethodGet)
+	router.HandleFunc("/device/update", updateDevices).
+		Methods(http.MethodPost).
+		Headers("Content-Type", "application/json")
 
 	srv := &http.Server{
 		Handler:      router,
 		Addr:         "127.0.0.1:8000",
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
+		IdleTimeout:  15 * time.Second,
 	}
 
-	log.Fatal(srv.ListenAndServe())
+	return srv
 }
