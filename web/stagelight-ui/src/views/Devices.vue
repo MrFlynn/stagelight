d4ac30d9a9c8521f87619ac35253c561c870c65d
MrFlynn <nick@pleatsikas.me>
2019-11-26 13:57:44 -0800

    Updated Device view device configuration options.
    This enables the user to set the color and the mode of the device in
    question.

--- web/stagelight-ui/src/views/Devices.vue
+++ web/stagelight-ui/src/views/Devices.vue
@@ -1,20 +1,37 @@
 <template>
-    <table class="primary">
-        <thead>
-            <tr>
-                <th>Device ID</th>
-                <th>Color</th>
-                <th>Mode</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr v-for="device in devices" v-bind:key="device.id">
-                <td>{{ device.id }}</td>
-                <td>{{ device.color }}</td>
-                <td>{{ device.mode }}</td>
-            </tr>
-        </tbody>
-    </table>
+    <div>
+        <table class="primary">
+            <thead>
+                <tr>
+                    <th>Device ID</th>
+                    <th>Color</th>
+                    <th>Mode</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="device in devices" v-bind:key="device.id">
+                    <td>{{ device.id }}</td>
+                    <td>
+                        <select>
+                            <option>{{ nameFromColor(device.colors[0]) }}</option>
+                            <option v-for="opt in getOtherItems(defaultColors, nameFromColor(device.colors[0]))" v-bind:key="opt">
+                                {{ opt }}
+                            </option>
+                        </select>
+                    </td>
+                    <td>
+                        <select>
+                            <option>{{ nameFromModeID(device.mode) }}</option>
+                            <option v-for="opt in getOtherItems(modes, nameFromModeID(device.mode))" v-bind:key="opt">
+                                {{ opt }}
+                            </option>
+                        </select>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <button>Submit</button>
+    </div>
 </template>
 
 <script>
@@ -24,7 +41,17 @@ export default {
     name: 'Devices',
     data () {
         return {
-            devices: []
+            devices: [],
+            defaultColors: {
+                'Red': 16711680,
+                'Green': 65280,
+                'Blue': 255,
+                'Off': 0
+            },
+            modes: {
+                'Normal': 0,
+                'Vote': 1
+            }
         }
     },
     created () {
@@ -32,6 +59,23 @@ export default {
         .then(response => {
             this.devices = response.data
         })
+    },
+    methods: {
+        swap: function (obj) {
+            var output = {}
+            Object.assign(output, ...Object.entries(obj).map(([a, b]) => ({[b]: a})))
+            return output
+        },
+        nameFromColor: function (color) {
+            return this.swap(this.defaultColors)[color]
+        },
+        nameFromModeID: function (mode) {
+            return this.swap(this.modes)[mode]
+        },
+        getOtherItems: function(obj, rem) {
+            var keys = Object.keys(obj)
+            return keys.filter(e => (e != rem))
+        }
     }
 }
 </script>
