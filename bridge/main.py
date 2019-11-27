590f86dc9bf42d09315ccee4f60f68a02d662ea1
MrFlynn <nick@pleatsikas.me>
2019-11-27 09:50:57 +0000

    Adding simple bride software.
    This version listens to a websocket from the web backend and issues
    commands to the bands over radio based on the data it recieves.

--- bridge/main.py
+++ bridge/main.py
@@ -1,16 +1,12 @@
 import adafruit_rfm9x
+import asyncio
 import board
 import busio
 import digitalio
+import json
+import sys
 import time
-
-console_message = """
-Please select from one of the following modes:
-0: Display color red.
-1: Display color green.
-2: Display color blue.
-3: Go into voting mode.
-"""
+import websockets
 
 
 def main() -> None:
@@ -21,21 +17,27 @@ def main() -> None:
     radio = adafruit_rfm9x.RFM9x(spi, chip_select, reset, 915.0)
     radio.tx_power = 23
 
-    while True:
-        try:
-            print(console_message)
-            mode = int(input("Please select a number [0-3]: "))
+    async def command_forwarder():
+        async with websockets.connect(sys.argv[1]) as ws:
+            while True:
+                content = await ws.recv()
+                devices = json.loads(content)
 
-            radio.send(bytes([mode]), tx_header=(1, 0, 0, 0))
-            print('Transmitting...')
+                for d in devices:
+                    packet = None
 
-            packet = radio.receive(with_header=True)
+                    color = d['colors'][0]
+                    if color == 0xFF0000:
+                        packet = 0
+                    elif color == 0x00FF00:
+                        packet = 1
+                    elif color == 0x0000FF:
+                        packet = 2
 
-            if packet:
-                print(f'Got vote: {packet}')
-        except KeyboardInterrupt:
-            print('\nGoodbye...')
-            raise SystemExit(0)
+                    if packet is not None:
+                        radio.send(bytes([packet]), tx_header=(1, 0, 0, 0))
+
+    asyncio.get_event_loop().run_until_complete(command_forwarder())
 
 if __name__ == '__main__':
     main()
