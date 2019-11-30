b26616c435205710e43b20dadfe8fc3e096cec0f
MrFlynn <nick@pleatsikas.me>
2019-11-30 00:36:11 -0800

    Added controller interface in Database.
    This should make this code far more modular and easier to add additional
    database controllers to. DBHandler merely acts as a frontend for the
    other controllers which do the actual database access.

--- web/api/database/database.go
+++ web/api/database/database.go
@@ -1,17 +1,34 @@
 package database
 
 import (
-	"encoding/json"
 	"fmt"
-	bolt "go.etcd.io/bbolt"
 	"log"
 	"os"
+
+	bolt "go.etcd.io/bbolt"
 )
+
+var subControllers = []Controller{&DeviceController{}}
+
+// Controller interface provides a list of methods that a controller should implement.
+type Controller interface {
+	Initialize(*bolt.DB) error
+
+	// Generic get methods.
+	Get(*bolt.DB, interface{}) (interface{}, error)
+	GetAll(*bolt.DB) ([]interface{}, error)
+	GetName() string
+
+	// Generic add/update methods
+	Add(*bolt.DB, []byte) error
+	AddMultiple(*bolt.DB, []byte) error
+}
 
 // DBHandler acts as an interface for bbolt.
 type DBHandler struct {
-	db   *bolt.DB
-	path string
+	db          *bolt.DB
+	controllers map[string]Controller
+	path        string
 }
 
 // New creates a new handler interface for bbolt.
@@ -26,126 +43,58 @@ func New(path string) DBHandler {
 		log.Fatalf("Could not open database: %s", path)
 	}
 
+	m := map[string]Controller{}
+	for _, c := range subControllers {
+		m[c.GetName()] = c
+	}
+
 	handler := DBHandler{
-		db:   db,
-		path: path,
+		db:          db,
+		controllers: m,
+		path:        path,
 	}
 
 	if !exists {
-		err := handler.initialize()
-		if err != nil {
-			log.Fatal(err)
+		for _, c := range handler.controllers {
+			c.Initialize(handler.db)
 		}
 	}
 
 	return handler
 }
 
-func (handler *DBHandler) initialize() error {
-	err := handler.db.Update(func(tx *bolt.Tx) error {
-		_, err := tx.CreateBucket([]byte("Devices"))
-		if err != nil {
-			return fmt.Errorf("Could not create bucket 'Devices'\n%s", err)
-		}
+// Get runs the Get interface method on the controller with the given name.
+func (h *DBHandler) Get(controller string, id interface{}) (interface{}, error) {
+	if c, ok := h.controllers[controller]; ok {
+		return c.Get(h.db, id)
+	}
 
-		return nil
-	})
-
-	return err
+	return nil, fmt.Errorf("Could not find controller with name %s", controller)
 }
 
-// GetDevice gets information about specific device from the database.
-func (handler *DBHandler) GetDevice(id uint8) (Device, error) {
-	device := &Device{}
+// GetAll runs the GetAll interface method on the controller with the given name.
+func (h *DBHandler) GetAll(controller string) ([]interface{}, error) {
+	if c, ok := h.controllers[controller]; ok {
+		return c.GetAll(h.db)
+	}
 
-	err := handler.db.View(func(tx *bolt.Tx) error {
-		c := tx.Bucket([]byte("Devices")).Cursor()
-
-		_, v := c.Seek([]byte{id})
-		if v == nil {
-			log.Printf("Could not find device with ID %d", id)
-			return fmt.Errorf("Could not find device with ID %d", id)
-		}
-
-		err := json.Unmarshal(v, device)
-		if err != nil {
-			return err
-		}
-
-		return nil
-	})
-
-	return *device, err
+	return nil, fmt.Errorf("Could not find controller with name %s", controller)
 }
 
-// GetAllDevices gets a list of all devices in the database.
-func (handler *DBHandler) GetAllDevices() ([]Device, error) {
-	devices := &[]Device{}
+// Add runs the Add interface method on the controller with the given name.
+func (h *DBHandler) Add(controller string, singleton []byte) error {
+	if c, ok := h.controllers[controller]; ok {
+		return c.Add(h.db, singleton)
+	}
 
-	err := handler.db.View(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte("Devices"))
-
-		err := b.ForEach(func(k, v []byte) error {
-			var dev Device
-
-			err := json.Unmarshal(v, &dev)
-			if err != nil {
-				log.Printf("Could not get device with ID %d", k[0])
-				return err
-			}
-
-			*devices = append(*devices, dev)
-			return nil
-		})
-
-		return err
-	})
-
-	return *devices, err
+	return fmt.Errorf("Could not find controller with name %s", controller)
 }
 
-// AddDevice adds a single empty device to the database.
-func (handler *DBHandler) AddDevice(id uint8) error {
-	return handler.db.Update(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte("Devices"))
+// AddMultiple runs the AddMultiple interface method on the controller with the given name.
+func (h *DBHandler) AddMultiple(controller string, set []byte) error {
+	if c, ok := h.controllers[controller]; ok {
+		return c.AddMultiple(h.db, set)
+	}
 
-		emptyDevice := Device{ID: id, Mode: Normal}
-		d, err := json.Marshal(&emptyDevice)
-		if err != nil {
-			return err
-		}
-
-		err = b.Put([]byte{id}, d)
-		if err != nil {
-			log.Printf("Could not create device with ID %d", id)
-			return err
-		}
-
-		log.Printf("Succesfully create device with ID %d", id)
-		return nil
-	})
-}
-
-// UpdateDevices updates a list of devices in the database.
-func (handler *DBHandler) UpdateDevices(devices []Device) error {
-	return handler.db.Batch(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte("Devices"))
-
-		for _, device := range devices {
-			d, err := json.Marshal(&device)
-			if err != nil {
-				return err
-			}
-
-			err = b.Put([]byte{device.ID}, d)
-			if err != nil {
-				log.Printf("Unable to update device with ID %d", device.ID)
-				return err
-			}
-
-			log.Printf("Sucessfully updated device with ID %d", device.ID)
-		}
-
-		return nil
-	})
+	return fmt.Errorf("Could not find controller with name %s", controller)
 }
