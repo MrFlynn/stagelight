e695272e057cd937bbe484ea19c275dcf8be0488
MrFlynn <nick@pleatsikas.me>
2019-12-11 22:17:30 -0800

    Adding page to display votes.

--- web/stagelight-ui/src/router/index.js
+++ web/stagelight-ui/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Device from '../views/Devices.vue'
+
+import Device from '@/views/Devices.vue'
+import Votes from '@/views/Votes.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +11,11 @@ const routes = [
     path: '/',
     name: 'home',
     component: Device
+  },
+  {
+    path: '/votes',
+    name: 'votes',
+    component: Votes
   }
 ]
 
