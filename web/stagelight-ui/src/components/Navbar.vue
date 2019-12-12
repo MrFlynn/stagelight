341dc27ee8063b98c1f855b2f14f5af5d1dc0d7e
MrFlynn <nick@pleatsikas.me>
2019-12-11 22:35:42 -0800

    Adding navigation bar.

---
+++ web/stagelight-ui/src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+    <nav class="navbar is-dark" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+            <h1 class="title is-4 navbar-item">Stagelight</h1>
+        </div>
+        <div id="navbarBasicExample" class="navbar-menu">
+            <div class="navbar-start">
+                <a href="/" class="navbar-item">Devices</a>
+                <a href="/votes" class="navbar-item">Votes</a>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'Navbar'
+}
+</script>
